81434e7ae0ff8f1f6ce1cb980414bda23412b147
duylee777 <duyleit98@gmail.com>
2026-09-14 18:23:29 +0000

    create dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
 
 Route::get('/auth/{provider}', [SocialController::class, 'redirectToProvider'])->name('social.login');
 Route::get('/auth/{provider}/callback', [SocialController::class, 'handleProviderCallback']);
